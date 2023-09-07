c9319483c59de6f1080879a8a6bef0615350a701
Rakshitha1505 <144348869+Rakshitha1505@users.noreply.github.com>
2023-09-07 18:18:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+udef isleap year(year):
+if (year%4==0 and year%100!=0):
+  return True
+  else
+  return False 
+year=2012
+if is leap year (year):
+print('{}is a leap year. '.format (year))
+else:
+print('{}is not a leap year. '.format(year))
